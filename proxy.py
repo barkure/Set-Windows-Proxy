9169cbfb0f936fdf4fe9fb9192975b22e60ce69c
barkure <43804451+barkure@users.noreply.github.com>
2024-06-19 22:18:54 +0800

    feat: 更新代理脚本以支持配置Git代理

--- proxy.py
+++ proxy.py
@@ -17,7 +17,7 @@ def enable_system_proxy(proxy_address, proxy_port):
         # 设置代理服务器
         winreg.SetValueEx(reg_key, "ProxyServer", 0, winreg.REG_SZ, proxy_string)
     
-    print(f"Proxy set to {proxy_string}")
+    print(f"system proxy set to {proxy_string}")
 
 def disable_system_proxy():
     # 打开注册表路径
@@ -28,7 +28,7 @@ def disable_system_proxy():
         # 设置代理禁用
         winreg.SetValueEx(reg_key, "ProxyEnable", 0, winreg.REG_DWORD, 0)
     
-    print("Proxy disabled")
+    print("system proxy disabled")
 
 def set_git_proxy(proxy_address, proxy_port):
     proxy_url = f"http://{proxy_address}:{proxy_port}"
@@ -36,31 +36,33 @@ def set_git_proxy(proxy_address, proxy_port):
     subprocess.run(['git', 'config', '--global', 'http.proxy', proxy_url], check=True)
     # 配置HTTPS代理
     subprocess.run(['git', 'config', '--global', 'https.proxy', proxy_url], check=True)
-    print("Git proxy has been configured successfully.")
+    print("git proxy has been configured successfully.")
 
 def remove_git_proxy():
     # 移除HTTP代理
     subprocess.run(['git', 'config', '--global', '--unset', 'http.proxy'], check=True)
     # 移除HTTPS代理
     subprocess.run(['git', 'config', '--global', '--unset', 'https.proxy'], check=True)
-    print("Git proxy has been removed successfully.")
+    print("git proxy has been removed successfully.")
 
 
 # 配置信息
-proxy_address = "192.168.31.46"
-proxy_port = "7890"
-git_proxy_address = "127.0.0.1"
-git_proxy_port = "7890"
+opi_proxy_address = "192.168.31.46"
+opi_proxy_port = "7890"
+clash_verge_proxy_address = "127.0.0.1"
+clash_verge_proxy_port = "7890"
 
 # 设置系统代理
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        if sys.argv[1] == "enable_system_proxy":
-            enable_system_proxy(proxy_address, proxy_port)
+        if sys.argv[1] == "enable_system_proxy_to_opi":
+            enable_system_proxy(opi_proxy_address, opi_proxy_port)
+        elif sys.argv[1] == "enable_system_proxy_to_clash_verge":
+            enable_system_proxy(clash_verge_proxy_address, clash_verge_proxy_port)
         elif sys.argv[1] == "disable_system_proxy":
             disable_system_proxy()
-        elif sys.argv[1] == "set_git_proxy":
-            set_git_proxy(git_proxy_address, git_proxy_port)
+        elif sys.argv[1] == "set_git_proxy_to_clash_verge":
+            set_git_proxy(clash_verge_proxy_address, clash_verge_proxy_port)
         elif sys.argv[1] == "remove_git_proxy":
             remove_git_proxy()
     else:
